8cf79e41f9f523efbfca637dee1faa050940efef
Alexandra <manik167@gmail.com>
2025-10-22 08:07:20 +0200

    firebase comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'pages/login_page.dart';
 import 'pages/module_selection_page.dart';
 //import 'pages/confirmation_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //await Firebase.initializeApp();
   runApp(ModuleChooseApp());
 }
 
